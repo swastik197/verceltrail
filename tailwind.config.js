06ffef8fb59e2f9e1f4d887a7d7e72f84ac4ec99
Swastik Sahu <sahuswastik50@gmail.com>
2025-01-22 00:27:54 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/*.ejs","./public/javascripts/*.js"],
+  theme: {
+    extend: {
+      theme:{
+        // colors:{
+          
+        // }
+        // colors: ({ colors }) => ({swastik:colors.rgb(27, 39, 43)})
+      }
+    },
+  },
+  plugins: [],
+}
+
